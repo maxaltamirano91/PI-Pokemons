0bdccaafb1788b0cae176c0b6f1acbfddda58ebe
Maximiliano Altamirano <maximiliano.altamirano@Flipdish-Maximiliano-Altamirano.local>
2024-05-15 11:26:41 +1200

    Modelos Pokemons y Type creados + Front end modificado a react

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,59 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "pokemon",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      image: {
+        type: DataTypes.BLOB,
+        // type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+
+      hp: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      attack: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      defense: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      speed: {
+        type: DataTypes.INTEGER,
+      },
+
+      height: {
+        type: DataTypes.INTEGER,
+      },
+
+      weight: {
+        type: DataTypes.INTEGER,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
