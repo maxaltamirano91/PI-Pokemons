f3951770eb838b7328d6469237ae7f3448748647
Maximiliano Altamirano <maximiliano.altamirano@Flipdish-Maximiliano-Altamirano.local>
2024-06-05 11:56:25 +1200

    axios agregado a main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import axios from "axios";
 
 import App from "./App";
 import "./index.css";
 import store from "./redux/store.js";
 
-// axios.defaults.baseURL = "http://localhost:3001/";
-axios.defaults.baseURL = "pi-pokemons-production-2abc.up.railway.app";
+axios.defaults.baseURL = "http://localhost:3001/";
+// axios.defaults.baseURL = "pi-pokemons-production-2abc.up.railway.app";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
